eae24ddd5058a0777d8ff26c63676be46490fdc5
JAEGEON LEE <worjs2964@gmail.com>
2023-03-08 15:17:46 +0900

    init

---
+++ src/main/java/link/mobilekiosk/mobilekiosk/MobileKioskApplication.java
@@ -0,0 +1,13 @@
+package link.mobilekiosk.mobilekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileKioskApplication.class, args);
+    }
+
+}
